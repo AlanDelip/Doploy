820855f76e8439a82cc618c5d03e669c8448a510
Zhufeng Xu <xzhufen@amazon.com>
2019-06-07 16:41:24 -0700

    [udpate] implement flask+mongodb

--- app/app.py
+++ app/app.py
@@ -1,17 +1,30 @@
-import os, json
-from flask import Flask, redirect, url_for, request, render_template, jsonify, Response
-from bson import json_util
+from flask import Flask
 from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 
-client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
+client = MongoClient('mongodb://db:27017/')
+
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def home():
-    return 'Hello World!'
+    db = client.test_database
+    collection = db.test_collection
+    post = {"author": "Mike",
+            "text": "My first blog post!",
+            "tags": ["mongodb", "python", "pymongo"],
+            "date": datetime.datetime.utcnow()}
+    collection.insert_one(post)
+
+    list = []
+    for post in collection.find():
+        list.append(post)
+
+    return str(list)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
